78c1a14219968fa61345e65ce8be66f684a75c86
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-04-02 22:01:49 -0400

    refactor: r script and notebook for saving data - move r markdown notebook code to script for saving data from sql - import csv file to r markdown

---
+++ analysis/save-mta-alerts.R
@@ -0,0 +1,19 @@
+library(DBI)
+library(tidyverse)
+library(lubridate)
+library(stringr)
+
+## Connect to db
+con <- dbConnect(drv=RSQLite::SQLite(), dbname="alerts.db")
+
+alerts <- dbReadTable(con, "mtaalerts")
+alerts <- select(alerts, -c(index, Agency))
+
+alerts <- alerts %>%
+  mutate(Message.Id = as.integer(Message.Id) ) %>%
+  mutate(Date = mdy_hm(Date)) %>%
+  mutate(elevator_escalator_id = str_match(Message, "(Elevator|Escalator) ((EL|ES)...x?)")[,3]) %>%
+  mutate(is_out_of_service = grepl('outage|Outage', Subject)) %>%
+  relocate(elevator_escalator_id, is_out_of_service)
+
+write.csv(alerts,"alerts.csv", row.names = FALSE)
