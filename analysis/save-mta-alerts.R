2563b3d2b1a25160f901e2289d0b8a3140dfd9d2
Ro Encarnacion <10480484+seerocode@users.noreply.github.com>
2021-04-16 00:22:00 -0400

    fix(analysis): fix regex for missing equipment numbers

--- analysis/save-mta-alerts.R
+++ analysis/save-mta-alerts.R
@@ -12,8 +12,9 @@ alerts <- select(alerts, -c(index, Agency))
 alerts <- alerts %>%
   mutate(Message.Id = as.integer(Message.Id) ) %>%
   mutate(Date = mdy_hm(Date)) %>%
-  mutate(elevator_escalator_id = str_match(Message, "(Elevator|Escalator) ((EL|ES)...x?)")[,3]) %>%
+  mutate(elevator_escalator_id = str_match(Message, "((EL|ES)\\d{2,3}(X|x)?)")[,2]) %>%
   mutate(is_out_of_service = grepl('outage|Outage', Subject)) %>%
   relocate(elevator_escalator_id, is_out_of_service)
+alerts
 
 write.csv(alerts,"alerts.csv", row.names = FALSE)
